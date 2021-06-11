846828b077fafbd3dfaff644603e12aae13ac9c4
Nikita Shidlowsky <nik-801@yandex.ru>
2021-06-11 22:39:39 +0300

    Fixed bugs: not working API

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import requests
 
 app = Flask(__name__, static_url_path="")
 
+
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify({'Error': 'Bad request'}), 400)
@@ -12,13 +13,12 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify({'Error': 'Not found'}), 404)
 
+
 @app.route('/gif/cute', methods=['GET'])
 def get_cute_gif():
     r = requests.get("https://evergene.io/api/hug")
-    print(r.json()['url'])
     return redirect(r.json()['url'], code=302)
 
 
 if __name__ == '__main__':
-    global connectionAuth, connectionEco
-    app.run('127.0.0.1', 80, app, use_reloader=True, use_debugger=True)
+    app.run(port=80)
